c10f24d65c4f48daa6088fd35efe5b9b4aa29aa4
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2020-02-08 23:31:51 -0600

    LowestCommonAncestor

---
+++ LowestCommonAncestor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LowestCommonAncestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lowest Common Ancestor (LCA)!");
+
+            
+        }
+
+        public static TreeNode LCA(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if(p.val < root.val && q.val < root.val)
+            {
+                LCA(root.left, p, q);
+            }else if(p.val >= root.val && q.val >= root.val)
+            {
+                LCA(root.right, p, q);
+            }
+            else
+            {
+                return root;
+            }
+
+            return root;
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        public int val;
+
+        TreeNode(int x) { val = x; }
+    }
+
+}
+
+}
