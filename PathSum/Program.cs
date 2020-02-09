7f1433a62dd95c4b92f3cddd58dac166d6762ef1
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2020-02-08 23:33:46 -0600

    PathSum

---
+++ PathSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PathSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int[] nums = { 4, 5, 7, 1, 2 };
+
+            Array.Sort(nums);
+
+        }
+
+        public bool hasPathSum(Node root, int sum)
+        {
+            if(root == null)
+            {
+                return false;
+            }
+            else if(root.left == null && root.right ==null && sum - root.val == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+            }
+        }
+        public class Node
+        {
+            public Node left;
+            public Node right;
+            public int val;
+
+            Node(int x) { val = x; }
+        }
+
+    }
+}
