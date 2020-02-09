d01d04f96f9e3b0cc36a07a10390f4355b5c3f54
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2020-02-08 23:24:45 -0600

    BFS_DFS

---
+++ BFS_DFS/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFS_DFS
+{
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public string data;
+
+        public Node(string data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Node(string data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+}
---
+++ BFS_DFS/dfs_traversal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFS_DFS
+{
+    partial class Program
+    {
+        static void dfs_traversal(Node node)
+        {
+            // Base case
+            if(node == null)
+                return;
+
+            Console.Write(node.data + " ");
+
+            // Recursive call
+            dfs_traversal(node.left);
+
+            //Console.Write(node.data + " ");
+            
+            dfs_traversal(node.right);
+
+            // Console.Write(node.data + " ");
+        }
+    }
+}
