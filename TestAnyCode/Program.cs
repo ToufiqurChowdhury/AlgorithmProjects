a42bf1665df9223764ca2f221eba7b1ffe8f7320
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2020-02-08 23:40:54 -0600

    TestAnyCode

---
+++ TestAnyCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestAnyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var itemTypeName = "assignment";
+
+            if (!char.IsUpper(itemTypeName[0]))
+            {
+                itemTypeName = itemTypeName[0].ToString().ToUpper() + itemTypeName.Substring(1);
+            }
+
+            Console.WriteLine(itemTypeName);
+        }
+    }
+}
