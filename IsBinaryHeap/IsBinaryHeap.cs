3228cba42a642d6f0c31d53ba1a632ce92375d66
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2020-02-08 23:29:00 -0600

    IsbinaryHeap

---
+++ IsBinaryHeap/IsBinaryHeap.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IsBinaryHeap
+{
+    class IsBinaryHeap
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 14, 15, 10, 7, 12, 2, 7, 3 };
+
+            int n = arr.Length - 1;
+
+            if (isHeap(arr, 0, n))
+            {
+
+                Console.WriteLine("Yes!");
+            }
+            else
+            {
+                Console.WriteLine("No!");
+            }
+        }
+
+        static bool isHeap(int[] arr, int i, int n)
+        {
+
+            if(i > (n - 2) / 2)
+            {
+                return true;
+            }
+
+            if(arr[i] >= arr[2*i+1] && arr[i] >= arr[2*i + 2] && isHeap(arr, 2*1+1, n) && isHeap(arr, 2*2+1, n))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
